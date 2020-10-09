3666848cc7fd5f51ce6a60b7d9c05b1d711561f9
manu <mechin.emmanuel@lilo.org>
2020-10-09 18:09:11 +0200

    Update primary and secondary colors of theme

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import axios from 'axios/index';
 
 import React, {Component} from 'react';
 
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+
 import './App.css';
 import { AppBar } from './components/AppBar';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
@@ -13,8 +15,18 @@ const theme = createMuiTheme({
             minHeight: 86,
             backgroundColor: 'white',
             color: '#5C5C5C'
-        }
+        },
     },
+    palette: {
+        primary: {
+            main: '#E4B231',
+            contrastText: '#fff',
+        },
+        secondary: {
+            main: '#69A95D',
+            contrastText: '#fff',
+        },
+    }
 });
 
 
