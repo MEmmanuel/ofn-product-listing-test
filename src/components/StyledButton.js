a1ffd2b51b26fd7be7a3d5491da8f156e4c69bca
manu <mechin.emmanuel@lilo.org>
2020-10-10 19:17:32 +0200

    Cleaning logs

--- src/components/StyledButton.js
+++ src/components/StyledButton.js
@@ -1,11 +1,11 @@
 import styled  from 'styled-components'
 import Button from "@material-ui/core/Button";
 
-export const StyledButton = styled(Button).attrs(({bold, uppercase, backgroundColor}) => {console.log('button', bold, uppercase);return ({
+export const StyledButton = styled(Button).attrs(({bold, uppercase, backgroundColor}) => ({
     bold: bold,
     uppercase: uppercase !== false,  // default is uppercase
     backgroundColor: backgroundColor,
-})})`
+}))`
     font-weight: ${props => props.bold};
     text-transform: ${props => props.uppercase ? "uppercase" : "unset"};
     background-color: ${props => props.backgroundColor};
--- src/components/StyledChip.js
+++ src/components/StyledChip.js
@@ -1,10 +1,10 @@
 import styled  from 'styled-components'
 import Chip from "@material-ui/core/Chip";
 
-export const StyledChip = styled(Chip).attrs(({bold, uppercase}) => {console.log('chips', bold, uppercase,uppercase === undefined);return ({
+export const StyledChip = styled(Chip).attrs(({bold, uppercase}) => ({
     bold: bold,
     uppercase: uppercase === true,  // default is lowercase
-})})`
+}))`
     font-weight: ${props => props.bold};
     text-transform: ${props => props.uppercase ? "uppercase" : "unset"};
 `;
